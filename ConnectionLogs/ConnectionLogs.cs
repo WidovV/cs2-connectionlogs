be10c0490d17cc8512ca8da29a58fa27101529b9
S <snr7@live.dk>
2023-12-13 17:08:27 +0100

    Add gitignore

--- ConnectionLogs/ConnectionLogs.cs
+++ ConnectionLogs/ConnectionLogs.cs
@@ -2,7 +2,6 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
-using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Cvars;
 using Nexd.MySQL;
